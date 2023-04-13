b745a9c03b432ac3fdb17186ff920f3ba5a58e72
SurajSharma13 <114924126+SurajSharma13@users.noreply.github.com>
2023-04-13 15:16:42 +0300

    Add files via upload

---
+++ Song.java
@@ -0,0 +1,14 @@
+
+
+package com.mycompany.song;
+
+
+
+
+public class Song {
+
+    public static void main(String[] args) throws ClassNotFoundException {
+         MainFrame Show = new MainFrame();
+         
+    }
+}
